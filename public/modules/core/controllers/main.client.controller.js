8ec9d72fcc39f43192b4532fd6a18042362a1524
RobertSnodgrass <robert.snodgrass@gmail.com>
2015-06-01 17:01:30 -0500

    (Feat/mainView): Added main route

--- public/modules/core/controllers/main.client.controller.js
+++ public/modules/core/controllers/main.client.controller.js
@@ -1,12 +1,24 @@
 'use strict';
 
+angular.module('core').controller('mainController', ['$scope', 'Menus',
+	function($scope, Menus) {
+		$scope.isCollapsed = false;
+		$scope.menu = Menus.getMenu('topbar');
 
+		$scope.toggleCollapsibleMenu = function() {
+			$scope.isCollapsed = !$scope.isCollapsed;
+		};
 angular.module('core').directive('youtube', 
 	function($window) {
 	  return {
 	    restrict: "E",
 
-
+		// Collapsing the menu after navigation
+		$scope.$on('$stateChangeSuccess', function() {
+			$scope.isCollapsed = false;
+		});
+	}
+]);
 	    scope: {
       		height:   "@",
       		width:    "@",
