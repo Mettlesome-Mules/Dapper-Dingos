799b63f08bb7925f282c3fa6b0e619ccd86e9e96
Dan Corman <dan.corman@gmail.com>
2015-06-08 15:05:34 -0500

    rebasing

--- public/modules/core/controllers/chat.client.controller.js
+++ public/modules/core/controllers/chat.client.controller.js
@@ -2,35 +2,36 @@
 angular.module('core')
 // #DD Establish functionality for pressing the enter key, 
 .directive('ngEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keypress", function (event) {
-            if(event.which === 13) {
-                scope.$apply(function (){
-                    scope.$eval(attrs.ngEnter);
-                });
-                event.preventDefault();
-            }
-        });
-    };
+	return function (scope, element, attrs) {
+		element.bind("keypress", function (event) {
+			if (event.which === 13) {
+				scope.$apply(function () {
+					scope.$eval(attrs.ngEnter);
+				});
+				event.preventDefault();
+			}
+		});
+	};
 })
+
 // #DD develop method to post methods to server and DB
-.factory('MessageFactory', ['$http', function ($http){
+.factory('MessageFactory', ['$http', function ($http) {
 	return {
 		postMessage: function (message, callback) {
-			console.log('Attempting Post')
+			console.log('Attempting Post');
 			$http.post('/message', message)
-			.success(function(data, status){
+			.success(function (data, status) {
 				callback(data, false);
 			}).
-			error(function(data, status) {
+			error(function (data, status) {
 				callback(data, true);
 			});
 		}
-	} 
+	};
 }])
 
 //#DD controller interface for user chat window, need to change userName to current authed user
-.controller('chatController', ['$scope','Authentication', 'MessageFactory', function ($scope, Authentication, MessageFactory) {
+.controller('chatController', ['$scope', 'Authentication', 'MessageFactory', function ($scope, Authentication, MessageFactory) {
 	$scope.userName = Authentication.user;
 	$scope.message = '';
 	$scope.filterText = '';
@@ -38,32 +39,24 @@ angular.module('core')
 
 	var socket = io.connect();
 
-	// #DD load previous messages from chat
-	socket.on('pastMessages', function (data) {
-		console.log(data, 'past')
-		$scope.messages = data.reverse();
-		// data.forEach(function (message) {
-		// 	$scope.messages.unshift(message);
-		// })
-		$scope.$apply();
-	});
+    // #DD load previous messages from chat
+  socket.on('pastMessages', function (data) {
+  	console.log(data, 'past');
+  	$scope.messages = data.reverse();
+  	$scope.$apply();
+  });
 
 
 
-	//#DD using the local authentication as a condition, send a message to the server
-	$scope.sendMessage = function () {
-		console.log("Send message event triggered")
-
-		var chatMessage = {
-			'username' : $scope.userName.displayName,
-			'message' : $scope.message
-		};
-
-		socket.emit('newMessage',chatMessage);
-		$scope.message = '';
-		};
-
-		socket.emit('getUsers')
+    //#DD using the local authentication as a condition, send a message to the server
+  $scope.sendMessage = function () {
+  	console.log("Send message event triggered");
+  	var chatMessage = {
+  		'username': $scope.userName.displayName,
+  		'message': $scope.message
+  	};
+  	socket.emit('newMessage', chatMessage);
+  	$scope.message = '';
+  };
+  socket.emit('getUsers');
 }]);
-
-
