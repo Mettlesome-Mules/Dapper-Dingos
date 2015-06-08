c51ff567a70caa12bd4230d99ef50cec564b4bf3
RobertSnodgrass <robert.snodgrass@gmail.com>
2015-06-08 11:12:33 -0500

    Removed unnecessary authorization strategies, editing files as needed

--- config/passport.js
+++ config/passport.js
@@ -27,7 +27,14 @@ module.exports = function() {
 	});
 
 	// Initialize strategies
-	config.getGlobbedFiles('./config/strategies/**/*.js').forEach(function(strategy) {
+	//#DD: As only Google auth strategy is being used, had to explicitly include it and local.js files
+	//#DD: All other strategies have been removed from App/Config/Strategies
+	//#DD: Express.js file was also edited to facilitate this
+	//#DD: Test.js, secure.js, development.js, and production.js were also modified
+	config.getGlobbedFiles('./config/strategies/**/google.js').forEach(function(strategy) {
+		require(path.resolve(strategy))();
+	});
+	config.getGlobbedFiles('./config/strategies/**/local.js').forEach(function(strategy) {
 		require(path.resolve(strategy))();
 	});
 };
