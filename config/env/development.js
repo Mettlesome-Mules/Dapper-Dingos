bc4243b0f848ba170bc058b71f383ff0bb175830
jmunsch <james.a.munsch@gmail.com>
2015-06-12 12:30:53 -0500

    added google oauth, switched to mongolab db

--- config/env/development.js
+++ config/env/development.js
@@ -1,16 +1,16 @@
 'use strict';
 // #DD - added the url for our mongolab hosted database
 module.exports = {
-	db: 'localhost:27017/socket4me',
-	// db: 'mongodb://dapperdingo:dapperpw@ds041032.mongolab.com:41032/dapper-dingosdb',
+	//db: 'localhost:27017/socket4me',
+	db: 'mongodb://mettle:neighneigh@ds033617.mongolab.com:33617/lounger',
 	app: {
-		title: 'Dapper-Dingos - Development Environment'
+		title: 'Lounger'
 	},
 	//#DD: Entries for Facebook, Twitter, Linked In, and Github have been removed
 	//#DD: These authorization stratgies were not utilized in the project
 	google: {
-		clientID: process.env.GOOGLE_ID || '514791028469-vtvgnb6evriqu757ph87gvf74crkfcv6.apps.googleusercontent.com',
-		clientSecret: process.env.GOOGLE_SECRET || 'Dvn7XJenZSfsQV9YcgLmhdBN',
+		clientID: process.env.GOOGLE_ID || '185307625644-ab2asrcg7pnj8pberl2680gtp04iidc3.apps.googleusercontent.com',
+		clientSecret: process.env.GOOGLE_SECRET || 'uCVPruNEtIIo2zbobsa-fxuV',
 		callbackURL: '/auth/google/callback'
 	},
 	mailer: {
